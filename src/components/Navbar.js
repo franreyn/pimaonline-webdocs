102fd1524ddc168538d87f0c4f633e3466bb2a7b
aphan-webdev <aphan@pima.edu>
2023-04-21 15:19:25 -0700

    Update mobile menu functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,34 +13,39 @@ const Navbar = () => {
     <>
       <nav className="wd-navbar">
         {/* logo */}
-        <div className="logo-wrapper d-block">
+        <div className="logo-wrapper">
           <Link href={"/index.html"}>
             <Image className="logo" src="/images/logo.svg" alt="" width={74} height={21} />
           </Link>
         </div>
-        <div className="wd-navwrap">
-          {/* hamburger button */}
-          <button className="mobile-btn" onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? (
-              <Image className="hamburger" src="/images/minus-solid.svg" alt="" width={24} height={24} />
-            ) : (
-              <Image className="hamburger" src="/images/bars-solid.svg" alt="" width={24} height={24} />
-            )}
-          </button>
-          {/* nav links */}
-          <div className={`${menuOpen ? 'show-menu' : 'hide-menu'}`}>
-            <ul className="nav-links">
-              <li>
-                <Link href={"/site/getting-started.html"}>Docs</Link>
-              </li>
-              <li>
-                <Link href={"/site/themes.html"}>Themes</Link>
-              </li>
-              <li>
-                <Link href="https://fontawesome.com/v5/search?m=free">Icons</Link>
-              </li>
-            </ul>
-          </div>
+        {/* hamburger button */}
+        <button className="mobile-btn">
+          <label for="check">
+            <input type="checkbox" id="check" onClick={() => setMenuOpen(!menuOpen)}/> 
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </button>
+        {/* nav links */}
+        <div className={`nav-container ${menuOpen ? 'show-menu' : 'hide-menu'}`}>
+          <ul className="nav-links">
+            <li>
+              <Link href={"/site/getting-started.html"}>Docs</Link>
+              <ul className="sub-items">
+                <li><Link href={"/site/getting-started.html"}>Getting Started</Link></li>
+                <li><Link href={"/site/layouts.html"}>Layouts</Link></li>
+                <li><Link href={"/site/widgets.html"}>Widgets</Link></li>
+                <li><Link href={"/site/utilities.html"}>Utilities</Link></li>
+              </ul>
+            </li>
+            <li>
+              <Link href={"/site/themes.html"}>Themes</Link>
+            </li>
+            <li>
+              <Link href="https://fontawesome.com/v5/search?m=free">Icons</Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
