b6c400f0089efb703301ef7cea7d80d2befa3caf
aphan-webdev <aphan@pima.edu>
2023-05-09 13:00:02 -0700

    Add homepage component to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
+import Homepage from "@/components/Homepage";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Head>
       <main>
         <Navbar />
-
+        <Homepage />
       </main>
     </>
   )
