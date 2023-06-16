7aadf5f8a5e9ece8f29989163d142c1e73229318
aphan-webdev <aphan@pima.edu>
2023-06-16 11:19:26 -0700

    Create carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+export default function Carousel() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handlePrevious = () => {
+    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  return (
+    <div className="carousel">
+      <div className="carousel-container">
+        <button className="previous-button" onClick={handlePrevious}>
+          <Image src="/images/chevron-left-solid.png" alt="Left arrow" width={36} height={36} />
+        </button>
+        <Image src={images[activeIndex].src} alt={images[activeIndex].alt} width={1390} height={855} />
+        <button className="next-button" onClick={handleNext}>
+          <Image src="/images/chevron-right-solid.png" alt="Right arrow" width={36} height={36} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const images = [
+  {
+    src: "/images/theme-2.png",
+    alt: "Snapshot of dental theme",
+  },
+  {
+    src: "/images/theme-1.png",
+    alt: "Snapshot of HR theme",
+  },
+  {
+    src: "/images/theme-3.png",
+    alt: "Snapshot of fitness theme",
+  },
+];
