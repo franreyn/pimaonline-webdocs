7a629ea613659cb62a215601126645602a9733c4
aphan-webdev <aphan@pima.edu>
2023-06-28 16:53:13 -0700

    Add widget components to widgets page

--- src/pages/widgets.js
+++ src/pages/widgets.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Navbar from '@/components/Navbar';
+import Script from 'next/script';
 import Sidebar from '@/components/Sidebar';
 import Assignments from "@/components/widgets/Assignments";
 import Blockquote from '@/components/widgets/Blockquote';
@@ -24,11 +25,20 @@ export default function Widgets() {
         <meta name="description" content="Pima Online Webdocs" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/night-owl.min.css" />
+        <Script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js" defer />
+        <Script src="https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/js/scripts2.js" defer />
+        <Script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js" />
+        <Script async>hljs.highlightAll();</Script>
       </Head>
       <header>
         <Navbar />
       </header>
       <div className="wd-grid">
+        <aside>
+          <Sidebar />
+        </aside>
         <main>
         <h1>Widgets</h1>
         <p>Widgets are individual components which together form your web page.</p>
@@ -47,9 +57,6 @@ export default function Widgets() {
         <VocabCards />
         <VocabList />
         </main>
-        <aside>
-          <Sidebar />
-        </aside>
       </div>
     </>
   )
