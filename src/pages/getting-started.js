4e88ed55e01349dd1cb122e9777f9ce56ac78fff
aphan-webdev <aphan@pima.edu>
2023-06-30 10:47:28 -0700

    Add footer component to pages

--- src/pages/getting-started.js
+++ src/pages/getting-started.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
+import Footer from '@/components/Footer';
 
 export default function GettingStarted() {
   return (
@@ -21,6 +22,9 @@ export default function GettingStarted() {
           <Sidebar />
         </aside>
       </div>
+      <footer>
+        <Footer />
+      </footer>
     </>
   )
 }
