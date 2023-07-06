d049e3e5e563d3c77327794a8a7ac427703901fb
Clint Mossman <cmossman@pima.edu>
2023-07-06 15:32:54 -0700

    Edit sidebar with links for layouts

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -33,9 +33,9 @@ export default function Sidebar() {
         </li>
         <li><Link href="layouts">Layouts</Link>
           <ul>
-            <li><Link href="one-column">One Column</Link></li>
-            <li><Link href="two-column">Two Column</Link></li>
-            <li><Link href="three-section">Three Section</Link></li>
+            <li><Link href="layouts/#one-column-layout" className="toc-one-column-layout">One Column</Link></li>
+            <li><Link href="layouts/#two-column-layout" className="toc-two-column-layout">Two Column</Link></li>
+            <li><Link href="layouts/#three-section-layout" className="toc-three-section-layout">Three Section</Link></li>
           </ul>
         </li>
         <li><Link href="widgets">Widgets</Link>
