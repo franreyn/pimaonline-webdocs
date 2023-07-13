d0efee79c351a0a16ae7223148155a0304bb6925
Clint Mossman <cmossman@pima.edu>
2023-07-13 08:50:30 -0700

    Added theme gallery page

--- src/pages/themes.js
+++ src/pages/themes.js
@@ -1,18 +1,70 @@
-import Head from "next/head";
-import Footer from "@/components/Footer";
+import themeArray from "../components/themeData";
+import styles from "../styles/themes.module.css"
+import Link from "next/link";
+import Navbar from "@/components/Navbar";
+import { useState, useLayoutEffect } from "react";
+import Sidebar from "@/components/Sidebar";
+import Head from 'next/head'
 
 export default function Themes() {
+
+  const [theme, setTheme] = useState();
+  const [name, setName] = useState();
+  const [description, setDescription] = useState();
+
+  const handleThemeClick = (selectedTheme, selectedName, selectedDescription) => {
+    setTheme(selectedTheme);
+    setName(selectedName);
+    setDescription(selectedDescription);
+  };
+
+  useLayoutEffect(() => {
+          // Remove any existing theme link
+          const existingLink = document.querySelector('link[data-theme-link]');
+          if (existingLink) {
+            document.head.removeChild(existingLink);
+          }
+  })
+
   return (
     <>
-      <Head>
-        <title>PimaOnline Webdocs</title>
+    <Head>
+    <title>Theme Gallery</title>
         <meta name="description" content="Pima Online Webdocs" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <footer>
-        <Footer />
-      </footer>
+    </Head>
+            <header>
+        <Navbar />
+      </header>
+      <div className="wd-grid">
+    <aside>
+      <Sidebar />
+    </aside>
+      <main>
+      <div className={styles.galleryGrid}>
+      {themeArray.map((item) => (
+            <div className={styles.galleryItem} key={item.theme}>
+              <div className={styles.overlay}>
+                <img src={item.image} />
+              </div>
+              <div className={styles.galleryInfo}>
+                <p>{item.name}</p>
+                <Link
+                  href={{
+                      pathname: "/theme",
+                  query: { ...item },
+                  }}
+                  onClick={() => handleThemeClick(item.theme, item.name, item.description)}
+               >
+                  View Theme
+                </Link>
+              </div>
+            </div>
+          ))}     
+           </div>
+      </main>
+      </div>
     </>
   )
 }
