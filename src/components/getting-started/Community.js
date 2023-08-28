59b0348156ab5512d6db83e5a447f40612d46e9b
aphan-webdev <aphan@pima.edu>
2023-08-28 16:41:36 -0700

    Update ids for sections

--- src/components/getting-started/Community.js
+++ src/components/getting-started/Community.js
@@ -1,8 +1,10 @@
 export default function Community() {
   return (
     <>
-      <h2 id="community" className="section-top">Community</h2>
-      <p className="mb-8">The PimaOnline ThemePack is maintained by the PimaOnline Web Design department. Questions, bug reports, or suggestions may be forwarded appropriately.</p>
+      <section id="toc-community">
+        <h2 id="community" className="section-top">Community</h2>
+        <p className="mb-8">The PimaOnline ThemePack is maintained by the PimaOnline Web Design department. Questions, bug reports, or suggestions may be forwarded appropriately.</p>
+      </section>
     </>
   )
 }
