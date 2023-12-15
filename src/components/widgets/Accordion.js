43a008b859c2a6e9c46767d524c96603ba55e6bc
aphan-webdev <107430634+aphan-webdev@users.noreply.github.com>
2023-12-15 21:10:35 +0000

    Add toggle classes to code snippet

--- src/components/widgets/Accordion.js
+++ src/components/widgets/Accordion.js
@@ -51,7 +51,7 @@ export default function Accordion() {
     <section className="wd-content" id="toc-accordion">
       <h2 id="accordion" className="section-top">Accordion</h2>
       <p>Use the <strong>Accordion</strong> to display content in collapsible compartments.</p>
-      <p>Use <span className="wd-monospace">&lt;div className="accordion"&gt;&lt;/div&gt;</span> to wrap your content.</p>
+      <p>Use <span className="wd-monospace">&lt;div class="accordion"&gt;&lt;/div&gt;</span> to wrap your content.</p>
       <div className="wd-window">
         <div className="wd-visual-ex">
           <div className="accordion">
@@ -68,14 +68,14 @@ export default function Accordion() {
             <code className="language-html" ref={codeRef}>
             {String.raw`<div class="accordion">
   <div class="accordion-item">
-    <h3 class="accordion-title arrow-down">Read</h3>
-    <div class="accordion-content">
+    <h3 class="accordion-title arrow-down toggle-btn">Read</h3>
+    <div class="accordion-content toggle-btn-content">
       <p>Making News, March/April 2018. By Deb Venasse, Reporter, <em>IBPA Independent</em> magazine</p>
     </div>
   </div>
   <div class="accordion-item">
-    <h3 class="accordion-title arrow-down">Discuss</h3>
-      <div class="accordion-content">
+    <h3 class="accordion-title arrow-down toggle-btn">Discuss</h3>
+      <div class="accordion-content toggle-btn-content">
         <p>Adweek is a well-known organization that covers media and communication people, places, and things. Some of its popular elements with journalists, creatives, and media workers are the newsletters with news about the actual people doing the media work.</p>
       </div>
   </div>
