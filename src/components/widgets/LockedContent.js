99b7fcc3ff296619f3ab33690127370c15caa410
Clint Mossman <cmossman@pima.edu>
2024-01-03 15:46:54 -0700

    Refactored code to be react friendly and use local state storage instead of local storage

--- src/components/widgets/LockedContent.js
+++ src/components/widgets/LockedContent.js
@@ -1,157 +1,62 @@
 import { useRef, useState, useEffect } from 'react';
 
 export default function LockedContent() {
+  // State variables
   const codeRef = useRef(null);
   const [buttonText, setButtonText] = useState('Copy code');
+  const [keyStatus, setKeyStatus] = useState(false);
 
-
+  // useEffect runs in the client side to listen for state changes and have access to the document object
   useEffect(() => {
-    // Query selectors to select elements by their CSS classes
-    const contentUnlockBtns = document.querySelectorAll(".unlock-btn");
+    const contentUnlockBtn = document.querySelector(".unlock-btn");
+
+    // Triggers the confirmation window for studnet to agree to terms
+    const handleUnlockButtonClick = () => {
+
+      // Saves the confirmation response from student
+      const confirmed = window.confirm(`Please confirm: ${contentUnlockBtn.textContent}`);
+
+      if (confirmed) {
+        // If use clicks ok, then change state variable keyStatus to true
+        setKeyStatus(true);
+      }
+    };
+
+    // Add click event listener to the unlock content button
+    contentUnlockBtn.addEventListener("click", handleUnlockButtonClick);
+
+    return () => {
+      // Cleanup: Remove event listener when the component is unmounted to avoid confirmation window being triggered twice
+      contentUnlockBtn.removeEventListener("click", handleUnlockButtonClick);
+    };
+  }, []);
+
+  // useEffect here to access document and listen for changes to keyStatus variable
+  useEffect(() => {
+    checkHiddenContent();
+  }, [keyStatus]);
+
+  const checkHiddenContent = () => {
+    // Target locked content areas and instruction areas
     const lockedContent = document.querySelectorAll(".locked-content");
     const contentLockInstructions = document.querySelectorAll(".instructions");
 
-    // Content Lock Widget
-    console.log("a")
-    console.log(contentUnlockBtns)
+    // For each locked content area, adjust visibility based on keyStatus
+    lockedContent.forEach((contentArea, index) => {
+      if (keyStatus) {
+        // Unlocks the content area
+        contentArea.classList.add("open");
+        // Removes instructions from view
+        contentLockInstructions[index].classList.add("complete");
+      } else {
+        // Locks content area (functionality to re-lock content isn't active currently)
+        contentArea.classList.remove("open");
 
-    // Save text content of unlock button
-    const unlockContent = [];
-
-    contentUnlockBtns.forEach((button) => {
-      unlockContent.push(button.textContent)
-    })
-
-    // Create an object that keeps track of keys and their statuses
-    let contentLockData = JSON.parse(localStorage.getItem("contentLockData")) || {};
-
-    if (lockedContent) {
-
-      // Checks the URL for the course number
-      // const currentURL = window.parent.location.href;
-      // const match = currentURL.match(/\/content\/(\d+)/);
-      // const courseNumber = match ? match[1] : null;
-
-      // Code for web docs to immitate the course number
-      const courseNumber = 1234;
-
-      // Add event listener for storage changes
-      window.addEventListener("storage", (event) => {
-        if (courseNumber) {
-          handleLocalStorageUpdate(event, courseNumber);
-        }
-      });
-
-      if (courseNumber) {
-
-        // Check if the course data object has the course number
-        if (!contentLockData.hasOwnProperty(courseNumber)) {
-          contentLockData[courseNumber] = {
-            keys: {},
-            id: 0,
-          };
-        } else {
-          // Reset the id count to 0 when the page is loaded
-          contentLockData[courseNumber].id = 0;
-        }
-
-        // Get key number for each content area
-        lockedContent.forEach((contentArea, index) => {
-          let keyNum = contentArea.getAttribute("data-key");
-
-          // Check if key already exists within the course, if it doesn't, add the key
-          if (!contentLockData[courseNumber].keys[keyNum]) {
-
-            // Add the key to the course keys
-            contentLockData[courseNumber].keys[keyNum] = false;
-          }
-        });
-
-
-        // Add event listener for unlock buttons outside the loop
-        contentUnlockBtns.forEach((button, index) => {
-          button.addEventListener("click", function (event) {
-
-            const keyNum = lockedContent[index].getAttribute("data-key");
-
-            // Add alert to ensure the user confirms the action to unlock the content
-            const confirmed = window.confirm(`Please confirm: ${unlockContent[index]}`)
-
-            if (confirmed) {
-              // Toggle the key status
-              contentLockData[courseNumber].keys[keyNum] = !contentLockData[courseNumber].keys[keyNum];
-
-              // Toggle classes based on key status
-              if (contentLockData[courseNumber].keys[keyNum]) {
-                lockedContent[index].classList.add("open");
-                contentLockInstructions[index].classList.add("complete");
-              } else {
-                lockedContent[index].classList.remove("open");
-                contentLockInstructions[index].classList.remove("complete");
-              }
-
-              // Save the updated contentLockData object to local storage
-              localStorage.setItem("contentLockData", JSON.stringify(contentLockData));
-
-              // Update the hidden content based on the key status
-              checkHiddenContent(courseNumber);
-            }
-          });
-
-          // Apply initial classes based on key status
-          const keyNum = lockedContent[index].getAttribute("data-key");
-
-          // Make sure contentLockData[courseNumber] is initialized
-          if (!contentLockData[courseNumber]) {
-            contentLockData[courseNumber] = {
-              keys: {},
-              id: 0
-            };
-          }
-
-          if (contentLockData[courseNumber].keys[keyNum]) {
-            lockedContent[index].classList.add("open");
-            contentLockInstructions[index].classList.add("complete");
-          }
-        }); // Add the { once: true } option);
-
-        // Call the checkHiddenContent function initially
-        checkHiddenContent(courseNumber);
-
-        // Save the updated contentLockData object to local storage
-        localStorage.setItem("contentLockData", JSON.stringify(contentLockData));
+        // Reveals instructions area (functionality to re-lock content isn't active currently)
+        contentLockInstructions[index].classList.remove("complete");
       }
-    }
-
-    // Function to run when local storage is updated
-    function handleLocalStorageUpdate(event, courseNumber) {
-      if (event && event.key === "contentLockData" && event.newValue) {
-        // Update contentLockData variable
-        contentLockData = JSON.parse(event.newValue);
-        // Run your code here
-        checkHiddenContent(courseNumber);
-      }
-    }
-
-    // Check content areas function
-
-    function checkHiddenContent(courseNumber) {
-
-      // Get key number for each content area
-      lockedContent.forEach((contentArea, index) => {
-        let keyNum = contentArea.getAttribute("data-key");
-        // Toggle classes based on key status
-        if (contentLockData[courseNumber].keys[keyNum]) {
-          lockedContent[index].classList.add("open");
-          contentLockInstructions[index].classList.add("complete");
-        } else {
-          lockedContent[index].classList.remove("open");
-          contentLockInstructions[index].classList.remove("complete");
-        }
-      });
-    }
-
-  })
+    });
+  };
 
   const handleCopyCode = () => {
     const codeElement = codeRef.current;
@@ -186,7 +91,7 @@ export default function LockedContent() {
       <div className="wd-window">
         <div className="wd-visual-ex">
           <div className="white-background">
-            <div className="locked-content" data-key="1">
+            <div className="locked-content">
               <h2>Partially Visible Area</h2>
               <p>This is some of the text and the beginning of the area that is hidden through the widget. As you continue the content becomes less and less visible. All the way until you can't really see what is underneath it forcing the student to have to unlock the content</p>
             </div>
