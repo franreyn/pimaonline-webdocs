2504a7ebaa228b6f50a44f0e0720e18f2213e4d4
joslinlee <146235383+joslinlee@users.noreply.github.com>
2024-07-31 17:35:38 -0700

    Update tips.js
    Switched all relevant ' to "

--- src/pages/tips.js
+++ src/pages/tips.js
@@ -1,16 +1,16 @@
-import Head from 'next/head';
-import Navbar from '@/components/Navbar';
-import Sidebar from '@/components/Sidebar';
-import Footer from '@/components/Footer';
-import { useLayoutEffect } from 'react';
-import Docs from '@/components/docsIntro';
-import Link from 'next/link';
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import Footer from "@/components/Footer";
+import { useLayoutEffect } from "react";
+import Docs from "@/components/docsIntro";
+import Link from "next/link";
 
 export default function Tips() {
 
   // Remove any existing theme link (required to keep theme styles just on the theme page)
   useLayoutEffect(() => {
-    const existingLink = document.querySelector('link[data-theme-link]');
+    const existingLink = document.querySelector("link[data-theme-link]");
     if (existingLink) {
       document.head.removeChild(existingLink);
     }
@@ -30,14 +30,14 @@ export default function Tips() {
       <div className="wd-grid">
         <main>
           <h2>Tips &amp; Tricks</h2>
-          <p className='wd-break'>Find practical insights and clever solutions to help you make the most of all that Themepack has to offer.</p>
+          <p className="wd-break">Find practical insights and clever solutions to help you make the most of all that Themepack has to offer.</p>
           <section className="wd-content" id="toc-tip-1">
           <h3 id="tip-1" className="section-top anchor">Utilizing Templates</h3>
           <p>Not sure where to start? Streamline your course creation process by leveraging <Link href="/templates" target="_blank" className="color-link">pre-made templates</Link>. A solid foundation, structured content and customizable elements, templates allow you to quickly and easily develop engaging and effective courses tailored to your needs.</p>
           <br />
           <div className="wd-subitems">
           <h4>How to Use Templates:</h4>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>Go to the templates page to browse through options</li>
               <li>Once you've chosen a template, copy the code and paste the supplied code</li>
               <li>Customize away!</li>
@@ -63,28 +63,28 @@ export default function Tips() {
           <section className="wd-side-by-side">
           <div className="wd-side-by-side-item">
           <h5>Accordion</h5>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>FAQs</li>
               <li>Course objectives or assignment details</li>
               <li>Digestible content sections</li>
             </ol>
             <br />
             <h5>Assignments</h5>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>Additional resources</li>
               <li>Reading highlights & links to those pages or chapters</li>
               <li>CLOs or course link shortcuts</li>
             </ol>
             <br />
             <h5>Border</h5>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>Featured content or important announcements</li>
               <li>Key takeaways or learning objectives</li>
               <li>Adding extra highlight to a blockquote</li>
             </ol>
             <br />
             <h5>Flip Cards</h5>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>Flash cards for quiz content</li>
               <li>Photos and expanded descriptions</li>
               <li>Contact cards or course resources</li>
@@ -92,7 +92,7 @@ export default function Tips() {
             </div>
             <div className="wd-side-by-side-item">
             <h5>Tables</h5>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>Course agenda or weekly assignments</li>
               <li>Historical event timelines</li>
               <li>Chapter summaries</li>
@@ -101,7 +101,7 @@ export default function Tips() {
             </ol>
             <br />
             <h5>Tabs</h5>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>Module or week breakdown</li>
               <li>Study materials & resources</li>
               <li>FAQs or instructions</li>
@@ -110,7 +110,7 @@ export default function Tips() {
             </ol>
             <br />
             <h5>Vocab Cards</h5>
-            <ol className='with-marker'>
+            <ol className="with-marker">
               <li>Glossary of terms or language translations</li>
               <li>Concept reviews and quiz prep</li>
               <li>Weekly updates and assignments</li>
