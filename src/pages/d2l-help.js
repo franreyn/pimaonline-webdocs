24662d50d8d3ce19cc5b44319eaff8c2883eaae2
Clint Mossman <cmossman@pima.edu>
2024-09-19 08:59:13 -0700

    Remove extra import

--- src/pages/d2l-help.js
+++ src/pages/d2l-help.js
@@ -7,7 +7,6 @@ import html from "highlight.js/lib/languages/xml";
 import { useEffect, useRef, useState, useLayoutEffect } from "react";
 import D2LHelpSidebar from "@/components/D2LHelpSidebar";
 import HowToAddTemplate from "@/components/d2l-help/HowToAddTemplate";
-import HowToAddContentBody from "@/components/d2l-help/AddingContentBody";
 
 export default function d2lHelp() {
 
