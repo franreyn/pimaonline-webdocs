47a4346d7b44aa64560331b648434862c3366365
Joslin Lee <jlee127@pima.com>
2024-10-10 14:53:14 -0700

    Reorganized links in the footer menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,34 +11,41 @@ export default function Footer() {
         </Link>
       </div>
         <ul>
-          <li>
-            <Link href="getting-started">Docs</Link>
+          <li><h4 className="accent">Build</h4>
             <ul className="sub-items">
               <li><Link href="getting-started">Getting Started</Link></li>
               <li><Link href="layouts">Layouts</Link></li>
+              <li><Link href="templates">Templates</Link></li>
+              <li><Link href="https://pimaonline-course-builder.netlify.app/">Course Builder</Link></li>
+            </ul>
+          </li>
+        </ul>
+        <ul>
+          <li><h4 className="accent">Customize</h4>
+          <ul className="sub-items">
+              <li><Link href="themes">Themes</Link></li>
+              <li><Link href="plugins#animation-library">Animation Library</Link></li>
+              <li><Link href="plugins#view-options-main">View Options</Link></li>
               <li><Link href="widgets">Widgets</Link></li>
               <li><Link href="utilities">Utilities</Link></li>
             </ul>
           </li>
         </ul>
         <ul>
-          <li><Link href="themes">Themes</Link></li>
-        </ul>
-        <ul>
-          <li>
-            Icons
+          <li><h4 className="accent">Help</h4>
               <ul className="sub-items">
-                <li><Link href="https://fontawesome.com/icons">FontAwesome</Link></li>
-                <li><Link href="https://www.gettyimages.com/">Getty Images</Link></li>
+                <li><Link href="tips">Tips &amp; Tricks</Link></li>
+                <li><Link href="help">D2L Help</Link></li>
               </ul>
           </li>
         </ul>
         <ul>
-          <li>
-            Community
+          <li><h4 className="accent">Quick Links</h4>
               <ul className="sub-items">
                 <li><Link href="https://github.com/">GitHub</Link></li>
                 <li><Link href="https://d2l.pima.edu/">D2L</Link></li>
+                <li><Link href="https://fontawesome.com/icons">FontAwesome</Link></li>
+                <li><Link href="https://www.gettyimages.com/">Getty Images</Link></li>
               </ul>
           </li>
         </ul>
