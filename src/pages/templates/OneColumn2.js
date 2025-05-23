474803b8c117ebf2bc8248a9c37a0076e79ef925
Joslin Lee <jlee127@pima.com>
2025-05-23 13:50:35 -0700

    Added toggle code to templates files

--- src/pages/templates/OneColumn2.js
+++ src/pages/templates/OneColumn2.js
@@ -10,9 +10,18 @@ import Image from "next/image";
 import TemplateSidebar from "@/components/TemplateSidebar";
 import BackToTop from "@/components/BackToTop";
 
+hljs.registerLanguage("html", html);
+
 export default function OneColumn2() {
   const codeRef = useRef(null);
   const [buttonText, setButtonText] = useState("Copy code");
+	const [showCode, setShowCode] = useState(false);
+
+	useEffect(() => {
+		if (showCode) {
+			hljs.highlightElement(codeRef.current); 
+		}
+	}, [showCode]);
 
   hljs.registerLanguage("html", html);
   useEffect(() => {
@@ -140,48 +149,18 @@ export default function OneColumn2() {
             expectations for the unit with organized clarity, using tables, accordions and
             assignment lists to help them prioritize tasks.
           </p>
-          <h3 className="spacer">Template Preview</h3>
-          <div className="template-preview">
-            <Image src={templateImage} alt="" width={800} height={1400} priority/>
-            <div>
-              <h4>Featured Widgets</h4>
-              <div className="wd-border">
-                <h5>Media Container</h5>
-                <p>Welcome students with an introductory video and keep it responsive for any device.</p>
-                <br />
-                <button className="wd-btn thin" onClick={changeToMedia}>Toggle in Preview</button>
-              </div>
-              <div className="wd-border">
-                <h5>Table</h5>
-                <p>Display tasks, due dates, and more in an organized and clear manner.</p>
-                <br />
-                <button className="wd-btn thin" onClick={changeToTable}>Toggle in Preview</button>
-              </div>
-              <div className="wd-border">
-                <h5>Side-by-Side</h5>
-                <p>Display two horizontal containers that can contain any content.</p>
-                <br />
-                <button className="wd-btn thin" onClick={changeToSideBySide}>Toggle in Preview</button>
-              </div>
-              <div className="wd-border">
-                <h5>Accordion</h5>
-                <p>Provide larger chunks of content a little at a time and let the student interact with the information.</p>
-                <br />
-                <button className="wd-btn thin" onClick={changeToAccordion}>Toggle in Preview</button>
-              </div>
-              <div className="wd-border">
-                <h5>Assignments</h5>
-                <p>List out the week's tasks in organized, interactive cards.</p>
-                <br />
-                <button className="wd-btn thin" onClick={changeToAssignments}>Toggle in Preview</button>
-              </div>
-            </div>
-          </div>
-          <br />
-          <div className="wd-window">
+					<br/>
+					<div className="wd-window">
             <div className="wd-btn-container">
-              <button className="wd-copy-btn" onClick={handleCopyCode}>{buttonText}</button>
+							<button
+						className="wd-toggle-btn"	onClick={() => setShowCode(!showCode)}>
+						{showCode ? "Hide code" : "Show code"}
+					</button>
+					{showCode && (
+					<button className="wd-copy-btn" onClick={handleCopyCode}>{buttonText}</button>
+					)}
             </div>
+						{showCode && (
             <div className="wd-html-code">
               <pre>
                 <code className="language-html" ref={codeRef}>
@@ -192,7 +171,6 @@ export default function OneColumn2() {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/css/themes/cards/styles.css">
 <link rel="stylesheet" type="text/css" href="custom.css">
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js" defer></script> 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@pimaonline/pimaonline-themepack/dist/js/scripts2.js" defer></script>
 <title>Course Intro</title>
 </head>
@@ -340,7 +318,46 @@ export default function OneColumn2() {
 `}</code>
               </pre>
             </div>
+						)}
           </div>
+          <h3 className="spacer">Template Preview</h3>
+          <div className="template-preview">
+            <Image src={templateImage} alt="" width={800} height={1400} priority/>
+            <div>
+              <h4>Featured Widgets</h4>
+              <div className="wd-border">
+                <h5>Media Container</h5>
+                <p>Welcome students with an introductory video and keep it responsive for any device.</p>
+                <br />
+                <button className="wd-btn thin" onClick={changeToMedia}>Toggle in Preview</button>
+              </div>
+              <div className="wd-border">
+                <h5>Table</h5>
+                <p>Display tasks, due dates, and more in an organized and clear manner.</p>
+                <br />
+                <button className="wd-btn thin" onClick={changeToTable}>Toggle in Preview</button>
+              </div>
+              <div className="wd-border">
+                <h5>Side-by-Side</h5>
+                <p>Display two horizontal containers that can contain any content.</p>
+                <br />
+                <button className="wd-btn thin" onClick={changeToSideBySide}>Toggle in Preview</button>
+              </div>
+              <div className="wd-border">
+                <h5>Accordion</h5>
+                <p>Provide larger chunks of content a little at a time and let the student interact with the information.</p>
+                <br />
+                <button className="wd-btn thin" onClick={changeToAccordion}>Toggle in Preview</button>
+              </div>
+              <div className="wd-border">
+                <h5>Assignments</h5>
+                <p>List out the week's tasks in organized, interactive cards.</p>
+                <br />
+                <button className="wd-btn thin" onClick={changeToAssignments}>Toggle in Preview</button>
+              </div>
+            </div>
+          </div>
+          <br />
         </main>
         <aside>
           <TemplateSidebar />
