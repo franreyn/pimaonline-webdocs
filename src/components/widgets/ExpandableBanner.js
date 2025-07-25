fd3bb280c4390fb6dd5a100a1baaefa9ae148897
aphan-webdev <aphan.webdev@gmail.com>
2025-07-25 13:12:13 -0700

    Add code snippets for different options

--- src/components/widgets/ExpandableBanner.js
+++ src/components/widgets/ExpandableBanner.js
@@ -40,10 +40,71 @@ export default function ExpandableBanner() {
       <p>Enabling the Expandable Banner will add a button to the banner image that allows users to expand the image to its full size. Clicking on the button opens an overlay that includes the full image and the optional caption/description.</p>
       <p>The Expandable Banner comes with a few options. Users can display:</p>
       <ol className="numbered-list">
-        <li>An image with <span className="wd-monospace">no description</span>.</li>
-        <li>An image that uses the <span className="wd-monospace">alt text as the description</span>. Alt text should be <span className="wd-monospace">short and concise</span>. If the description is longer and more detailed, use the next option.</li>
-        <li>An image with a <span className="wd-monospace">detailed description</span> that uses the <span className="wd-monospace">data-description attribute</span>.</li>
+        <li>An image with <span className="wd-monospace">no description</span>.
+          <div className="wd-btn-container">
+            <button className="wd-copy-btn" onClick={handleCopyCode}>
+              {buttonText}
+            </button>
+          </div>
+          <div className="wd-html-code">
+            <pre>
+              <code className="language-html" ref={codeRef}>
+                {String.raw`// This option has no alt text and no data-description attribute
+<header className="header expandable-banner">
+  <img src="/images/widgets/expandable-banner/portrait-art.jpg" alt="" />
+  <div class="text-container">
+    <h1>Module 1: Lorem ipsum dolor</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+  </div>
+</header>`}
+              </code>
+            </pre>
+          </div>        
+        </li>
+        <li>An image that uses the <span className="wd-monospace">alt text as the description</span>. Alt text should be <span className="wd-monospace">short and concise</span>. If the description is longer and more detailed, use the next option.
+          <div className="wd-btn-container">
+            <button className="wd-copy-btn" onClick={handleCopyCode}>
+              {buttonText}
+            </button>
+          </div>
+          <div className="wd-html-code">
+            <pre>
+              <code className="language-html" ref={codeRef}>
+                {String.raw`// This option contains a short alt text that will display as a caption
+<header className="header expandable-banner">
+  <img src="/images/widgets/expandable-banner/portrait-art.jpg" alt="The Salon of Hercules ceiling painting in the Palace of Versailles" />
+  <div class="text-container">
+    <h1>Module 1: Lorem ipsum dolor</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+  </div>
+</header>`}
+              </code>
+            </pre>
+          </div> 
+        </li>
+        <li>An image with a <span className="wd-monospace">detailed description</span> that uses the <span className="wd-monospace">data-description attribute</span>.
+          <div className="wd-btn-container">
+            <button className="wd-copy-btn" onClick={handleCopyCode}>
+              {buttonText}
+            </button>
+          </div>
+          <div className="wd-html-code">
+            <pre>
+              <code className="language-html" ref={codeRef}>
+                {String.raw`// This option also contains a more detailed data-description attribute that will display as a caption
+<header className="header expandable-banner">
+  <img src="/images/widgets/expandable-banner/portrait-art.jpg" alt="The Salon of Hercules ceiling painting in the Palace of Versailles" data-description="The Salon of Hercules ceiling painting in the Palace of Versailles, completed by François Lemoyne in 1736, is a grand Baroque masterpiece titled The Apotheosis of Hercules. Covering an expansive 1,400 square feet, the fresco depicts Hercules ascending to Mount Olympus, welcomed by the gods in a dramatic and dynamic composition filled with swirling clouds, rich colors, and expressive figures." />
+  <div class="text-container">
+    <h1>Module 1: Lorem ipsum dolor</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+  </div>
+</header>`}
+              </code>
+            </pre>
+          </div> 
+        </li>
       </ol>
+      <h3>Expandable banner sample</h3>
       <div className="wd-window">
         <div className="wd-visual-ex">
           <header className="header expandable-banner">
