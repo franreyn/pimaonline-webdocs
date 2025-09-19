25843bd7273eb766e84fd14221a2708626c24d98
Clint Mossman <cmossman@pima.edu>
2025-09-19 10:31:07 -0700

    Added alt text

--- src/components/plugins/MouseHoverHighlight.js
+++ src/components/plugins/MouseHoverHighlight.js
@@ -9,14 +9,14 @@ hljs.registerLanguage("html", html);
 export default function MouseHoverHighlight() {
   const codeRef = useRef(null);
   const [buttonText, setButtonText] = useState("Copy code");
-	const [showCode, setShowCode] = useState(false);
+  const [showCode, setShowCode] = useState(false);
 
-	useEffect(() => {
-		if (showCode) {
-			hljs.highlightElement(codeRef.current); 
-		}
-	}, [showCode]);
-	
+  useEffect(() => {
+    if (showCode) {
+      hljs.highlightElement(codeRef.current);
+    }
+  }, [showCode]);
+
   const handleCopyCode = () => {
     const codeElement = codeRef.current;
     const range = document.createRange();
@@ -38,16 +38,18 @@ export default function MouseHoverHighlight() {
       <section className="wd-content" id="toc-mouse-highlight">
         <h3 id="mouse-highlight" className="section-top anchor">Mouse Hover Highlight</h3>
         <p>Use <span className="wd-monospace">mouse-highlight</span> to add the mouse hover highlight view option to your course shell. This gives students the ability to highlight the line of text that the mouse hovers over, making reading large bodies of text easy to read and more engaging.</p>
-				<div className="wd-window">
-					<div className="wd-btn-container">
-						<button
-						className="wd-toggle-btn"	onClick={() => setShowCode(!showCode)}>
-						{showCode ? "Hide code" : "Show code"}
-					</button>
-					{showCode && (
-					<button className="wd-copy-btn" onClick={handleCopyCode}>{buttonText}</button>
-					)}          </div>
-					{showCode && (
+        <div className="wd-window">
+          <div className="wd-btn-container">
+            <button
+              className="wd-toggle-btn"
+              onClick={() => setShowCode(!showCode)}>
+              {showCode ? "Hide code" : "Show code"}
+            </button>
+            {showCode && (
+              <button className="wd-copy-btn" onClick={handleCopyCode}>{buttonText}</button>
+            )}
+          </div>
+          {showCode && (
           <div className="wd-html-code">
             <pre>
               <code className="language-html" ref={codeRef}>
@@ -55,17 +57,17 @@ export default function MouseHoverHighlight() {
               </code>
             </pre>
           </div>
-					)}
-          </div>
-					<br/>
-				<div className="wd-visual-ex">
-        <section className="wd-side-by-side">
-        <div className="wd-side-by-side-item">
-         <Image src="/images/plugins/VO-mouse-highlight.png" alt="" width={292} height={280} priority="true"  />
-          </div>
-          <div className="wd-side-by-side-item">
-          <Image src="/images/plugins/VO-mouse-highlight-on.png" alt="" width={292} height={280} />
-          </div>
+          )}
+        </div>
+        <br />
+        <div className="wd-visual-ex">
+          <section className="wd-side-by-side">
+            <div className="wd-side-by-side-item">
+              <Image src="/images/plugins/VO-mouse-highlight.png" alt="page with mouse highlight view option open" width={292} height={280} priority="true" />
+            </div>
+            <div className="wd-side-by-side-item">
+              <Image src="/images/plugins/VO-mouse-highlight-on.png" alt="page with mouse highlight view option active" width={292} height={280} />
+            </div>
           </section>
           </div>
 
